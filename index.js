098e5fbe32d7e41ded249f180b3507364985fb0f
AubreyHeartA <arian.aubreyheart18@gmail.com>
2024-05-24 11:42:17 +0800

    Edit Mutation

--- index.js
+++ index.js
@@ -12,33 +12,31 @@ const resolvers = {
         venues() {
           return db.venues;
         },
-        venue(_, { id }) {
-          return db.venues.find((venue) => venue.id === id);
+        venue(_, args) {
+          return db.venues.find((venue) => venue.id === args.id);
         },
         events() {
           return db.events;
         },
-        event(_, { id }) {
-          return db.events.find((event) => event.id === id);
+        event(_, args) {
+          return db.events.find((event) => event.id === args.id);
         },
         tickets() {
           return db.tickets;
         },
-        ticket(_, { id }) {
-          return db.tickets.find((ticket) => ticket.id === id);
+        ticket(_, args) {
+          return db.tickets.find((ticket) => ticket.id === args.id);
         },
         attendees() {
           return db.attendees;
         },
-        attendee(_, { id }) {
-          return db.attendees.find((attendee) => attendee.id === id);
+        attendee(_, args) {
+          return db.attendees.find((attendee) => attendee.id === args.id);
         }
       },
+    // nested queries
     Event: {
         venues: (parent) => parent.venues.map((id) => db.venues.find((v) => v.id === id)),
-        // venues(parent) {        
-        //     return parent.venues.map(itemId => db.venues.find(item => item.id === itemId))
-        // }
     },
     Ticket: {
         events: (parent) => parent.events.map((id) => db.events.find((e) => e.id === id)),
@@ -46,6 +44,47 @@ const resolvers = {
     Attendee: {
         tickets: (parent) => parent.tickets.map((id) => db.tickets.find((t) => t.id === id)),
     },
+    Mutation: {
+        updateVenue(_, args) {
+            db.venues = db.venues.map((v) => {
+                if (v.id === args.id) {
+                    return {...v, ...args.edits}
+                }
+
+                return v
+            })
+
+            return db.venues.find((v) => v.id === args.id);
+        },
+        updateEvent(_, args) {
+            db.events = db.events.map((e) => {
+                if (e.id === args.id) {
+                    return {...e, ...args.edits}
+                }
+                return e
+            })
+            return db.events.find((e) => e.id === args.id);
+        },
+        updateTicket(_, args) {
+            db.tickets = db.tickets.map((t) => {
+                if (t.id === args.id) {
+                    return {...t, ...args.edits}
+                }
+                return t
+            })
+            return db.tickets.find((t) => t.id === args.id);
+        },
+        updateAttendee(_, args) {
+            db.attendees = db.attendees.map((a) => {
+                if (a.id === args.id) {
+                    return {...a, ...args.edits}
+                }
+                return a
+            })
+            return db.attendees.find((a) => a.id === args.id);
+        }
+    }
+
 };
 
 
--- schema.js
+++ schema.js
@@ -1,42 +1,67 @@
 export const typeDefs = `#graphql
-  type Venue {
-    id: ID!
-    name: String!
-    location: String!
-  }
+    type Venue {
+        id: ID!
+        name: String!
+        location: String!
+    }
 
-  type Event {
-    id: ID!
-    name: String!
-    date: String!
-    venues: [Venue!]!
-  }
+    type Event {
+        id: ID!
+        name: String!
+        date: String!
+        venues: [Venue!]!
+    }
 
-  type Ticket {
-    id: ID!
-    events: [Event!]!
-    seatNo: String!
-  }
+    type Ticket {
+        id: ID!
+        events: [Event!]!
+        seatNo: String!
+    }
 
-  type Attendee {
-    id: ID!
-    name: String!
-    email: String!
-    tickets: [Ticket!]!
-  }
+    type Attendee {
+        id: ID!
+        name: String!
+        email: String!
+        tickets: [Ticket!]!
+    }
 
-  type Query {
-    venues: [Venue!]!
-    venue(id: ID!): Venue
-    events: [Event!]!
-    event(id: ID!): Event
-    tickets: [Ticket!]!
-    ticket(id: ID!): Ticket
-    attendees: [Attendee!]!
-    attendee(id: ID!): Attendee
-  }
+    type Query {
+        venues: [Venue!]!
+        venue(id: ID!): Venue
+        events: [Event!]!
+        event(id: ID!): Event
+        tickets: [Ticket!]!
+        ticket(id: ID!): Ticket
+        attendees: [Attendee!]!
+        attendee(id: ID!): Attendee
+    }
 
-  type Mutation {
-    
-  }
+    type Mutation {
+        updateVenue(id: ID!, edits: EditVenueInput!): Venue
+        updateEvent(id: ID!, edits: EditEventInput!): Event
+        updateTicket(id: ID!, edits: EditTicketInput!): Ticket
+        updateAttendee(id: ID!, edits: EditAttendeeInput!): Attendee
+    }
+
+    input EditVenueInput{
+        name: String
+        location: String
+    }
+
+    input EditEventInput{
+        name: String
+        date: String
+        venues: [ID!]
+    }
+
+    input EditTicketInput{
+        events: [ID!]
+        seatNo: String
+    }
+
+    input EditAttendeeInput{
+        name: String
+        date: String
+        tickets: [ID!]
+    }
 `
